55fbc364c5998909b35cbd0b85435d3295a35501
Heather Rusnica <heather.rusnica@gmail.com>
2017-10-03 13:48:55 -0400

    here

---
+++ m2-w6d1-dao-exercises-pair/src/test/java/com/techelevator/projects/view/JDBCDepartmentDAOTest.java
@@ -0,0 +1,34 @@
+package com.techelevator.projects.view;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JDBCDepartmentDAOTest {
+
+	@Test
+	public void testGetAllDepartments() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSearchDepartmentsByName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateDepartmentName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateDepartment() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDepartmentById() {
+		fail("Not yet implemented");
+	}
+
+}
